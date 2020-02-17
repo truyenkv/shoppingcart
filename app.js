25daa29015de2a87d91456910ea3e21a604b5495
truyenkv <kieuvantruyen.sgu@gmail.com>
2020-02-17 22:32:10 +0700

    user header

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-    res.send('working');
+    res.render('index');
 });
 
 //start the server
