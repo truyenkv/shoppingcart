7740234a6bf9246701fdaa478b2b064de0cfb783
truyenkv <kieuvantruyen.sgu@gmail.com>
2020-02-23 22:20:53 +0700

    done video 10

---
+++ models/page.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+//page schema
+var PageSchema = mongoose.Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    slug: {
+        type: String
+    },
+    content: {
+        type: String,
+        require: true
+    },
+    sorting: {
+        type: Number
+    }
+});
+var Page = module.exports = mongoose.model('Page', PageSchema);
